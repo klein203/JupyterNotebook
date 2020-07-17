4ec8629662b97444adf72ace5aef9e023a64db64
xusheng <klein203@gmail.com>
2020-07-17 18:16:19 +0800

    update mdp

--- Reinforcement_Learning/MDP.py
+++ Reinforcement_Learning/MDP.py
@@ -4,25 +4,25 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 import numpy as np
 
 
-class Observation(object):
-    def __init__(self, state, *args, **kwargs):
-        self.state = state
-        self.terminal = False
+# class State(object):
+#     def __init__(self, val, *args, **kwargs):
+#         self.val = val
+#         self.terminal = False
 
-    def set_terminal(self, terminal):
-        self.terminal = terminal
+#     def set_terminal(self, terminal):
+#         self.terminal = terminal
 
-    @property
-    def is_terminal(self):
-        return self.terminal
+#     @property
+#     def is_terminal(self):
+#         return self.terminal
 
-    def __str__(self):
-        return self.state
+#     def __str__(self):
+#         return self.state
 
 
-class Action(object):
-    def __init__(self, *args, **kwargs):
-        pass
+# class Action(object):
+#     def __init__(self, *args, **kwargs):
+#         pass
 
 
 class Policy(object):
@@ -33,7 +33,8 @@ class Policy(object):
         self.actions_space = actions_space
         self.n_actions = len(self.actions_space)
 
-        self.transit_matrix = np.
+        # self.transit_matrix = np.
+
 
     def next_action(self, s):
         # random policy
@@ -44,21 +45,24 @@ class Policy(object):
 
 
 class MDP(object):
-    def __init__(self, gamma=.9, *args, **kwargs):
+    def __init__(self, states_space, actions_space, probability_matrix, rewards, discount_factor=.9, *args, **kwargs):
         # states space
-        self.states_space = []
+        self.states_space = states_space
 
         # actions space
-        self.actions_space = []
+        self.actions_space = actions_space
 
-        # policy π(s, a)
-        self.policy = Policy()
+        # matrix
+        self.probability_matrix = self._norm(probability_matrix)
+
+        # rewards
+        self.rewards = rewards
 
         # gamma in [0, 1]
-        self.discounted_rate = gamma
+        self.gamma = discount_factor
     
     def r(self, s, a, s_):
-        """transient reward when state s become state s_ with action a."""
+        """reward function r(s, a, s_)."""
         
         # simple example: if meets terminal return 1, else 0
         if self.check_states_space():
@@ -90,11 +94,35 @@ class MDP(object):
 
     def check_actions_space(self, a):
         return a in self.actions_space
+    
+    def _norm(self, matrix):
+        return matrix
 
 
 
 
 if __name__ == "__main__":
-    states_space = [Observation(i) for i in range(9)]
+    n_col, n_row = 3, 3
+    states_space = ['(%d, %d)' % (i % n_col, i // n_col) for i in range(n_row * n_col)]
+    actions_space = ['left', 'right', 'up', 'down']
 
-    mdp = MDP()
+    p_matrix = [
+      # 00 10 20 01 11 21 02 12 22
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],    # (0, 0)
+        [10, 1, 1, 0, 1, 0, 0, 0, 0],    # (1, 0)
+        [0, 1, 1, 0, 0, 1, 0, 0, 0],    # (2, 0)
+        [1, 0, 0, 1, 1, 0, 1, 0, 0],    # (0, 1)
+        [0, 1, 0, 1, 0, 1, 0, 1, 0],    # (1, 1)
+        [0, 0, 1, 0, 1, 1, 0, 0, 1],    # (2, 1)
+        [0, 0, 0, 1, 0, 0, 1, 1, 0],    # (0, 2)
+        [0, 0, 0, 0, 1, 0, 1, 1, 1],    # (1, 2)
+        [0, 0, 0, 0, 0, 1, 0, 1, 1],    # (2, 2)
+    ]
+    # p_matrix = np.zeros((len(states_space), len(states_space)))
+    p_matrix = np.matrix(p_matrix)
+    p_matrix = p_matrix / p_matrix.mean(axis=1)
+
+    # mdp = MDP()
+    print(p_matrix)
+
+    
